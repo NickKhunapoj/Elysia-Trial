e10f95b28a3771bd69b63fa9f78333e0409b34ed
NickKhunapoj <Nickkipad@gmail.com>
2024-08-22 00:16:17 +0700

    Fix folder path

---
+++ src/index.ts
@@ -0,0 +1,172 @@
+import { Elysia, t } from "elysia";
+import {
+  getBooks,
+  getBook,
+  createBook,
+  deleteBook,
+  updateBook,
+  createUser,
+  getUser,
+} from "./model";
+import { jwt } from "@elysiajs/jwt";
+import { swagger } from "@elysiajs/swagger";
+
+
+const app = new Elysia().use(
+  jwt({
+    name: "jwt",
+    secret: process.env.JWT_SECRET || "secret",
+  })
+)
+.derive(async ({ jwt, cookie: { auth } }) => {
+  const profile = await jwt.verify(auth.value)
+  return {
+      profile
+  } 
+})
+.use(swagger())
+
+// ---------------- Guard Hook ---------------- //
+
+app.guard( {
+  beforeHandle({ set, profile }) {
+    console.log('before router')
+      if (!profile)
+          return (set.status = 'Unauthorized')
+  }
+}, app => {
+
+// ---------------- Get Books API ---------------- //
+
+app.get("/books", () => getBooks());
+
+// ---------------- Get Book By ID API ---------------- //
+
+app.get("/books/:id", async ({ params, profile}) => {
+  console.log('profile', profile)
+  return getBook(parseInt(params.id))
+});
+
+// ---------------- Insert Books API ---------------- //
+
+app.post(
+  "/books",
+  ({ body, set }) => {
+    const bookBody: any = body;
+    const response = createBook(
+      bookBody.title,
+      bookBody.author,
+      bookBody.price
+    );
+    if (response.status === "error") {
+      set.status = 400;
+      return { message: "Incomplete input" };
+    }
+    return { message: "ok" };
+  },
+  {
+    body: t.Object({
+      title: t.String(),
+      author: t.String(),
+      price: t.Number(),
+    }),
+  }
+);
+
+// ---------------- Update Books API ---------------- //
+
+app.put("/books/:id", ({ params, body, set }) => {
+  try {
+    const bookBody: any = body;
+    const bookId: number = parseInt(params.id);
+    const response = createBook({
+      title: bookBody.title,
+      author: bookBody.author,
+      price: bookBody.price,
+    });
+    if (response.status === "error") {
+      set.status = 400;
+      return { message: "Incomplete input" };
+    }
+    return { message: "ok" };
+  } catch (error) {
+    set.status = 500;
+    return { message: "Internal Server Error" };
+  }
+});
+
+// ---------------- Delete Books API ---------------- //
+
+app.delete("/books/:id", ({ params }) => {
+  const bookId: number = parseInt(params.id);
+  const response = deleteBook(bookId);
+  if (response.status === "error") {
+    set.status = 500;
+    return { message: "Unable to Delete" };
+  }
+  return { message: "Delete bookId: " + bookId };
+});
+
+
+return app
+})
+
+// ---------------- User Register API ---------------- //
+
+app.post(
+  "/register",
+  async ({ body }) => {
+    try {
+      let userData: any = body;
+      userData.password = await Bun.password.hash(userData.password, {
+        algorithm: "bcrypt",
+        cost: 4,
+      });
+      createUser(userData.email, userData.password);
+      return { message: "created successfully" };
+    } catch (error) {
+      set.status = 500;
+      return { message: "error", error };
+    }
+  },
+  {
+    body: t.Object({
+      email: t.String(),
+      password: t.String(),
+    }),
+  }
+);
+
+// ---------------- User Login API ---------------- //
+
+app.post("/login", async ({ body, set, jwt, cookie: { auth }, params }) => {
+  try {
+    let userData: any = body;
+    const response = await getUser(userData);
+    if (!response.loggedIn) {
+      set.status = 401;
+      return { message: "Invalid login" };
+    }
+
+    auth.set({
+      value: await jwt.sign(params),
+      httpOnly: true,
+      maxAge: 7 * 86400,
+      path: "/",
+  })
+
+    return { 
+      message: "Logged in successfully",
+      token: auth.value
+    };
+
+  } catch (error) {
+    set.status = 500;
+    return { message: "error", error };
+  }
+});
+
+app.listen(8000);
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
---
+++ src/model.ts
@@ -0,0 +1,119 @@
+import { Database } from "bun:sqlite";
+import { t } from "elysia";
+
+const db = new Database("mydb.sqlite");
+
+// Books
+
+const getBook = (id: number) => {
+  try {
+    const query = db.query("select * from books where id = $id;");
+    return query.get({ $id: id });
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+const getBooks = () => {
+  try {
+    const query = db.query("select * from books;");
+    return query.all();
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+const createBook = (title: string, author: string, price: number) => {
+  try {
+    if (!title || !author || !price) {
+      throw new Error("Incomplete input");
+    }
+    const query = db.query(
+      "insert into books (title, author, price) values ($title, $author, $price);"
+    );
+    return query.run({ $title: title, $author: author, $price: price });
+    return { status: "ok" };
+  } catch (error) {
+    console.log("error", error);
+    return { status: "error", error };
+  }
+};
+
+const updateBook = (id: number, book: any) => {
+  try {
+    const query = db.query(
+      "update books set title = $title, author = $author, price = $price where id = $id;"
+    );
+    return query.run({
+      $title: book.title,
+      $author: book.author,
+      $price: book.price,
+    });
+    return { status: "ok" };
+  } catch (error) {
+    console.log("error", error);
+    return { status: "error", error };
+  }
+};
+
+const deleteBook = (id: number) => {
+  try {
+    const query = db.query("delete from books where id = $id;");
+    return query.run({ $id: id });
+    return { status: "ok" };
+  } catch (error) {
+    console.log("error", error);
+    return { status: "error", error };
+  }
+};
+
+// Users
+
+const createUser = (email: string, password: string) => {
+  try {
+    const query = db.query(
+      "insert into users (email, password) values ($email, $password);"
+    );
+    return query.run({ $email: email, $password: password });
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+const getUser = async (user: any) => {
+  try {
+    const query = db.query(
+      "select * from users where email = $email;"
+    );
+    const userData = query.get({
+      $email: user.email,
+    });
+
+    if(!userData){
+      throw new Error("User not found");
+    }
+    const isMatch = await Bun.password.verify(user.password, userData.password)
+
+    if(!isMatch){
+      throw new Error("Invalid credentials");
+    }
+    return {
+      loggedIn: true,
+    };
+  } catch (error) {
+    console.log("error", error);
+    return {
+      loggedIn: false,
+    };
+  }
+};
+
+export {
+  getBook,
+  getBooks,
+  createBook,
+  updateBook,
+  deleteBook,
+  createUser,
+  getUser
+};
